74f13af7f10bc90babfcb10bafd60071e9f88ec6
kakomoma <alejandromorenomartin1990@gmail.com>
2024-06-25 20:11:36 +0200

    Responsive - Iteraction II

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,3 @@
-/* ESTRUCTURA JS 
-
-1. Navigation
-  1.1 Menu (appears and dissapear)
-  1.2 Button - index-hero (Salto a sección)  
-  1.3 Button - progress bar (Barra de progreso y go top) 
-  1.4 Scroller - toolstack-scroller
-  1.5 Tab - experience-education 
-  1.6 Carousel - carousel-project-hero  
-
 /* ----------------- NAVIGATION ----------------- */
 
 /* 1.2 Button - index-hero (Salto a sección) */
@@ -32,6 +22,18 @@ document.addEventListener('DOMContentLoaded', function () {
     scrollToSection();
   });
 });
+
+/* ----------------------------------------------- */
+
+function showSidebar(){
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'flex'
+}
+
+function hideSidebar(){
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'none'
+}
 
 /* ----------------------------------------------- */
 
@@ -106,128 +108,6 @@ function resumeAnimation(event) {
 
 /* ----------------------------------------------- */
 
-/* 1.5 Tab - experience-education */
-
-function changeTab(tab) {
-  // Obtener todos los botones de pestañas
-  var tabButtons = document.getElementsByClassName("tab-button");
-
-  // Eliminar la clase 'active' de todos los botones
-  for (var i = 0; i < tabButtons.length; i++) {
-    tabButtons[i].classList.remove("active");
-  }
-
-  // Agregar la clase 'active' al botón clicado
-  var activeButton = document.getElementById(tab + "Btn");
-  activeButton.classList.add("active");
-
-  // Llamar a la función showTab con el nombre de la pestaña
-  showTab(tab);
-}
-
-function showTab(tabName) {
-  // Oculta todos los bloques de contenido
-  var tabs = document.querySelectorAll('.tab-content');
-  tabs.forEach(function (tab) {
-    tab.classList.remove('active');
-  });
-
-  // Muestra el bloque de contenido correspondiente a la pestaña seleccionada
-  document.getElementById(tabName).classList.add('active');
-}
-
-/* ----------------------------------------------- */
-
-/* 1.6 Carousel - carousel-project-hero */
-
-/*const track = document.querySelector('.carousel__track');
-const slides = Array.from(track.children);
-const nextButton = document.querySelector('.carousel__button--right');
-const prevButton = document.querySelector('.carousel__button--left');
-const dotsNav = document.querySelector('.carousel__nav');
-const dots = Array.from(dotsNav.children);
-const slideWidth = slides[0].getBoundingClientRect().width;
-
-// arrange the slides next to one another
-// slides[0].style.left = slideWidth * 0 + 'px';
-// slides[1].style.left = slideWidth * 1 + 'px';
-// slides[2].style.left = slideWidth * 2 + 'px';
-
-const setSlidePosition = (slide, index) => {
-  slide.style.left = slideWidth * index + 'px';
-};
-slides.forEach(setSlidePosition);
-
-const moveToSlide = (track, currentSlide, targetSlide) => {
-  track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
-  currentSlide.classList.remove('current-slide');
-  targetSlide.classList.add('current-slide');
-}
-
-const updateDots = (currentDot, targetDot) => {
-  currentDot.classList.remove('current-slide');
-  targetDot.classList.add('current-slide');
-}
-
-const hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
-  if (targetIndex === 0) {
-    prevButton.classList.add('is-hidden');
-    nextButton.classList.remove('is-hidden');
-  } else if (targetIndex === slides.length - 1) {
-    prevButton.classList.remove('is-hidden');
-    nextButton.classList.add('is-hidden');
-  } else {
-    prevButton.classList.remove('is-hidden');
-    nextButton.classList.remove('is-hidden');
-  }
-}
-
-// when I click right, move slides to the right
-nextButton.addEventListener('click', e => {
-  const currentSlide = track.querySelector('.current-slide');
-  const nextSlide = currentSlide.nextElementSibling;
-  const currentDot = dotsNav.querySelector('.current-slide');
-  const nextDot = currentDot.nextElementSibling;
-  const nextIndex = slides.findIndex(slide => slide === nextSlide);
-
-  moveToSlide(track, currentSlide, nextSlide);
-  updateDots(currentDot, nextDot);
-  hideShowArrows(slides, prevButton, nextButton, nextIndex);
-})
-
-
-// when I click left, move slides to the left
-prevButton.addEventListener('click', e => {
-  const currentSlide = track.querySelector('.current-slide');
-  const prevSlide = currentSlide.previousElementSibling;
-  const currentDot = dotsNav.querySelector('.current-slide');
-  const prevDot = currentDot.previousElementSibling;
-  const prevIndex = slides.findIndex(slide => slide === prevSlide);
-
-  moveToSlide(track, currentSlide, prevSlide);
-  updateDots(currentDot, prevDot);
-  hideShowArrows(slides, prevButton, nextButton, prevIndex);
-})
-
-
-// when I click the nav indicators, move to that slide
-dotsNav.addEventListener('click', e => {
-  // what indicator was clicked on?
-  const targetDot = e.target.closest('button');
-
-  if (!targetDot) return;
-
-  const currentSlide = track.querySelector('.current-slide');
-  const currentDot = dotsNav.querySelector('.current-slide');
-  const targetIndex = dots.findIndex(dot => dot === targetDot);
-  const targetSlide = slides[targetIndex];
-
-  moveToSlide(track, currentSlide, targetSlide);
-  updateDots(currentDot, targetDot);
-  hideShowArrows(slides, prevButton, nextButton, targetIndex);
-}) */
-
-/* ----------------------------------------------- */
 
 /* [Table of content - article] */
 
